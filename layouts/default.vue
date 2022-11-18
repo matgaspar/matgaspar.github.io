2c2952c73ff7481286c8e4c7943d419579209a7c
Matheus <matheus@web7online.com>
2022-11-18 18:59:22 -0300

    Initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+    <header>
+        <nav>
+            <ul>
+                <li><NuxtLink to="/">Home</NuxtLink></li>
+                <li><NuxtLink to="/about">Sobre</NuxtLink></li>
+            </ul>
+        </nav>
+    </header>
+    <slot></slot>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1>Home</h1>
+    <input type="text" v-model="txtInputValue">
+    {{ txtInputValue }}
+</template>
+
+<script setup>
+    definePageMeta({
+        layout: "default"
+    })
+
+    let txtInputValue = ref(null);
+
+    let isLoad = ref(true);
+</script>
